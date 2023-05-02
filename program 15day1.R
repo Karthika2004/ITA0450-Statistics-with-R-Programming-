e7bbd5bc909abc7882e6f600549c0df3ca15c05a
Karthika2004 <112481430+Karthika2004@users.noreply.github.com>
2023-05-02 20:48:58 +0530

    Add files via upload

---
+++ program 15day1.R
@@ -0,0 +1,14 @@
+# create three vectors of different data types
+numeric_vec <- c(1, 2, 3, 4, 5)
+character_vec <- c("apple", "banana", "cherry")
+logical_vec <- c(TRUE, FALSE, TRUE, FALSE)
+
+# display the content of the vectors
+print(numeric_vec)
+print(character_vec)
+print(logical_vec)
+
+# display the data type of the vectors
+print(typeof(numeric_vec))
+print(typeof(character_vec))
+print(typeof(logical_vec))
